c8b32a02167f8d2078ad294c9f4859dcec644b2e
vsaj <openjuice@proton.me>
2022-07-05 08:01:29 -0500

    Add ModInitializer.

--- src/main/java/me/vsaj/exrecipe/ExRecipe.java
+++ src/main/java/me/vsaj/exrecipe/ExRecipe.java
@@ -1,4 +1,13 @@
 package me.vsaj.exrecipe;
 
-public class ExRecipe {
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExRecipe implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Extended Recipes starting up!");
+    }
 }
